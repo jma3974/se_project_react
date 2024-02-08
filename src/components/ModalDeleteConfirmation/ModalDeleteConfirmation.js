f4cfb78204bdb48840f12d43e47a19c382397889
jma3974 <jma3974@gmail.com>
2024-02-07 17:40:51 -0800

    P11 finalized delete confirmation 2/7/24 C

--- src/components/ModalDeleteConfirmation/ModalDeleteConfirmation.js
+++ src/components/ModalDeleteConfirmation/ModalDeleteConfirmation.js
@@ -14,28 +14,36 @@ const ModalDeleteConfirmation = ({
   return (
     <div
       className="modal__delete"
-      // buttonText="Yes, delete item"
+     
       onClose={handleCloseModal}
       isOpen={isOpen}
       onSubmit={handleSubmit}
     >
       <div className="modal__delete_block">
+
+      <button
+          className="modal__close modal__preview_close"
+          type="button"
+          onClick={handleCloseModal}
+        />
+
+
         <div className="modal__delete_content">
           <div className="modal__delete_content-text">
             Are you sure you want to delete this item? <br />
             This action is irreversible.
           </div>
-          <div className="modal__delete_content-text">
+          <div>
             <button
               type="text"
               className="modal__deleteConfirmation_button modal__deleteConfirmation_button-delete"
-            >
+            onclick={onDeleteItem}>
               Yes, delete this item
             </button>
           </div>
 
-          <div className="modal__delete_content-text">
-            <button type="text" className="modal__deleteConfirmation_button">Cancel</button>
+          <div>
+            <button type="text" className="modal__deleteConfirmation_button" onClick={handleCloseModal}>Cancel</button>
             
             
             </div>
--- src/utils/weatherAPI.js
+++ src/utils/weatherAPI.js
@@ -34,5 +34,8 @@ export const parseWeatherData = (data) => {
   return weather;
 };
 
+
+
+
 // weather.temperature.F = `${Math.round(data.main.temp)}°F`;
 // weather.temperature.C = `${Math.round((data.main.temp - 32) * 5/9)}°C`;
